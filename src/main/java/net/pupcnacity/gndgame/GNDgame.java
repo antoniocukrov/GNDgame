be1a79ee7954c72f870ec61f6d7fb45a57ed420f
antoniocukrov <antonio.cukrov1@gmail.com>
2022-05-13 11:35:15 +0200

    Initial commit

---
+++ src/main/java/net/pupcnacity/gndgame/GNDgame.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package net.pupcnacity.gndgame;
+
+/**
+ *
+ * @author 38595
+ */
+public class GNDgame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
