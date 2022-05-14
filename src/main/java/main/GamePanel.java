da5bfd37f3145a00a0157e9665c43711956728bb
antoniocukrov <antonio.cukrov1@gmail.com>
2022-05-14 19:00:58 +0200

    Tiles, Map, background!

--- src/main/java/main/GamePanel.java
+++ src/main/java/main/GamePanel.java
@@ -12,6 +12,7 @@ import java.awt.Graphics2D;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JPanel;
+import tile.TileManager;
 
 /**
  *
@@ -23,23 +24,18 @@ public class GamePanel extends JPanel implements Runnable{
     final int scale = 3;
     
     public final int tileSize = originalTilesize*scale; // 48x48 tile
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol;    // 768 pixels
     final int screenHeight = tileSize * maxScreenRow;   // 576 pixels
     
     //FPS
     int FPS = 60;
-    
+    TileManager tileM= new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     Player player = new Player(this,keyH);
     
-    //Set player's default position
-    
-    int playerX=100;
-    int playerY=100;
-    int playerSpeed=4;
     
     public GamePanel() { 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -125,6 +121,7 @@ public class GamePanel extends JPanel implements Runnable{
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2= (Graphics2D)g;
+        tileM.draw(g2);
         player.draw(g2);
         g2.dispose();
     }
---
+++ src/main/java/tile/TileManager.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tile;
+
+import java.awt.Graphics2D;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import javax.imageio.ImageIO;
+import main.GamePanel;
+
+/**
+ *
+ * @author 38595
+ */
+public class TileManager {
+
+    GamePanel gp;
+    Tile[] tile;
+    int mapTileNum[][];
+
+    public TileManager(GamePanel gp) {
+        this.gp = gp;
+
+        tile = new Tile[10];
+        mapTileNum=new int[gp.maxScreenCol][gp.maxScreenRow];
+        getTileImage();
+        loadMap();
+    }
+
+    public void getTileImage() {
+        try {
+            tile[0] = new Tile();
+            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/old/grass.png"));
+            tile[1] = new Tile();
+            tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/old/wall.png"));
+            tile[2] = new Tile();
+            tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/old/water.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void loadMap(){
+        
+        try{
+            InputStream is = getClass().getResourceAsStream("/maps/map01.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            int col = 0;
+            int row= 0;
+            while(col<gp.maxScreenCol && row<gp.maxScreenRow){
+                String line = br.readLine();
+                while(col<gp.maxScreenCol){
+                    String numbers[] = line.split(" ");
+                    
+                    int num = Integer.parseInt(numbers[col]);
+                    
+                    mapTileNum[col][row]=num;
+                    col++;
+                }
+                if(col==gp.maxScreenCol) {
+                    col=0;
+                    row++;
+                }
+            }
+            br.close();
+        } catch(IOException exe){
+            exe.printStackTrace();
+        }
+    }
+    
+    public void draw(Graphics2D g2){
+            int col = 0;
+            int row = 0;
+            int x = 0;
+            int y = 0;
+            
+            while(col < gp.maxScreenCol && row < gp.maxScreenRow) {
+                int tileNum = mapTileNum[col][row];
+                
+                g2.drawImage(tile[tileNum].image,x,y,gp.tileSize,gp.tileSize,null);
+                col++;
+                x+=gp.tileSize;
+                
+                if(col==gp.maxScreenCol){
+                    col=0;
+                    x=0;
+                    row++;
+                    y +=gp.tileSize;
+                }
+                
+            }
+        }
+}
