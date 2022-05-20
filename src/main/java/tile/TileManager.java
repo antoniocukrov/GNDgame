f08b7405607aa32882230ca4dd7d902bd15cc7e7
antoniocukrov <antonio.cukrov1@gmail.com>
2022-05-20 20:03:25 +0200

    Better looks

--- src/main/java/tile/TileManager.java
+++ src/main/java/tile/TileManager.java
@@ -34,17 +34,17 @@ public class TileManager {
     public void getTileImage() {
         try {
             tile[0] = new Tile();
-            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/old/grass.png"));
+            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/new/grass01.png"));
             tile[1] = new Tile();
-            tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/old/wall.png"));
+            tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/new/wall.png"));
             tile[1].collision = true;
             tile[2] = new Tile();
-            tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/old/water.png"));
+            tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/new/water00.png"));
             tile[2].collision = true;
             tile[3] = new Tile();
-            tile[3].image = ImageIO.read(getClass().getResourceAsStream("/tiles/old/earth.png"));
+            tile[3].image = ImageIO.read(getClass().getResourceAsStream("/tiles/new/earth.png"));
             tile[4] = new Tile();
-            tile[4].image = ImageIO.read(getClass().getResourceAsStream("/tiles/old/tree.png"));
+            tile[4].image = ImageIO.read(getClass().getResourceAsStream("/tiles/new/tree.png"));
             tile[4].collision = true;
             tile[5] = new Tile();
             tile[5].image = ImageIO.read(getClass().getResourceAsStream("/tiles/old/sand.png"));
